d66bd6d223b8b80e499f7f0ff8de0c757eb61992
Alex Kiura <kiuraalex@gmail.com>
2023-11-16 21:22:21 +0300

    Fetch all books

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from rizz_wsgi.api import API
+from storage import BookStorage
 
 
 app = API()
+book_storage = BookStorage()
+book_storage.create(name="Mastery", author="Robert Greene")
 
 
 @app.route("/", allowed_methods=["get"])
 def index(request, response):
-    response.html = app.template("index.html")
+    books = book_storage.all()
+    response.html = app.template("index.html", context={"books": books})
