9ef2b44d50cabe4a4dc31b7ae1b449c6b26ce29a
Alex Kiura <kiuraalex@gmail.com>
2023-11-16 22:03:47 +0300

    Add protected book creation route

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from rizz_wsgi.api import API
 from storage import BookStorage
 from book_factory import test_books
-from auth import STATIC_TOKEN
-
+from auth import login_required, STATIC_TOKEN, TokenMiddleware
+from rizz_wsgi.response import Response
 
 app = API()
 book_storage = BookStorage()
 book_storage.bulk_create(test_books)
+
+app.add_middleware(TokenMiddleware)
 
 
 @app.route("/", allowed_methods=["get"])
@@ -18,3 +20,12 @@ def index(request, response):
 @app.route("/login", allowed_methods=["post"])
 def login(request, response):
     response.json = {"token": STATIC_TOKEN}
+
+
+@app.route("/books", allowed_methods=["post"])
+@login_required
+def create_book(request, response: Response):
+    book = book_storage.create(**request.POST)
+
+    response.status_code = 201
+    response.json = book._asdict()
