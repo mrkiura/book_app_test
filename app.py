bdc50e3afc6f53b7dbcf6a0a83fe88ed01cabae4
Alex Kiura <kiuraalex@gmail.com>
2023-11-16 21:55:45 +0300

    Add login route

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from rizz_wsgi.api import API
 from storage import BookStorage
 from book_factory import test_books
+from auth import STATIC_TOKEN
 
 
 app = API()
@@ -12,3 +13,8 @@ book_storage.bulk_create(test_books)
 def index(request, response):
     books = book_storage.all()
     response.html = app.template("index.html", context={"books": books})
+
+
+@app.route("/login", allowed_methods=["post"])
+def login(request, response):
+    response.json = {"token": STATIC_TOKEN}
